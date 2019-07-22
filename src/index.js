9467a1e9dec1ec5c62fb224fc1131d2e078f0076
magerlinc <mikkel_ac@hotmail.com>
2019-07-22 13:36:24 +0200

    Exported dragScrollBar correctly

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import Draggable from './components/draggable';
 import Droppable from './components/droppable';
 import DragDropContext from './components/drag_drop_context';
+import DragScrollBar from './components/drag_scroll_bar';
 import VirtualizedScrollBar from './components/virtualized-scrollbar';
 import ExampleBoard from './examples/example-board';
 import DynamicHeightExample from './examples/example-dynamic';
 import ExampleMultipleDroppables from './examples/example-multiple-droppables';
 
-export {Draggable, Droppable, DragDropContext, VirtualizedScrollBar, ExampleBoard, DynamicHeightExample, ExampleMultipleDroppables};
+export {Draggable, Droppable, DragDropContext, DragScrollBar, VirtualizedScrollBar, ExampleBoard, DynamicHeightExample, ExampleMultipleDroppables};
